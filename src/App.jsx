e6bc99f3fb8b2f0dfcd10cd4110e3e404243ab0a
Qutub Ahmed <qutubahmedkhan@outlook.com>
2025-07-16 21:51:05 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  const addValue = ()=>{
+    if(counter < 10) {
+      counter = counter + 1;
+    }
+    setCounter(counter)
+    setCounter((c)=> c + 1)
+  }
+  const removeValue = ()=>{
+    if(counter > 0) {
+      counter = counter - 1;
+    }
+    setCounter(counter)
+    setCounter((c)=> c - 1)
+  }
+
+  return (
+    <>
+      <h1>Chai or react</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button style={{marginBlockEnd: '20px'}} onClick={addValue}>add value</button>
+      <br />
+      <button onClick={removeValue}>decrease value</button>
+    </>
+  )
+}
+
+export default App
